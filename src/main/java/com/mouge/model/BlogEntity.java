8b93827329c35f34afd1df9f521b69354e1d3186
jaf <code_writer@163.com>
2016-08-02 21:58:57 +0800

    BlogEntity.java UserEntity.java

---
+++ src/main/java/com/mouge/model/BlogEntity.java
@@ -0,0 +1,88 @@
+package com.mouge.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by jaf on 16/8/2.
+ */
+@Entity
+@Table(name = "blog", schema = "springmvc", catalog = "")
+public class BlogEntity {
+    private int id;
+    private String title;
+    private String content;
+    private UserEntity userByUserId;
+    private UserEntity userByUserId1;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title", nullable = false, length = 100)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(name = "content", nullable = true, length = 255)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogEntity that = (BlogEntity) o;
+
+        if (id != that.id) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (content != null ? !content.equals(that.content) : that.content != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    public UserEntity getUserByUserId() {
+        return userByUserId;
+    }
+
+    public void setUserByUserId(UserEntity userByUserId) {
+        this.userByUserId = userByUserId;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    public UserEntity getUserByUserId1() {
+        return userByUserId1;
+    }
+
+    public void setUserByUserId1(UserEntity userByUserId1) {
+        this.userByUserId1 = userByUserId1;
+    }
+}
