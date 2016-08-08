72c603f74f4464e9513d9c59b2bf52381bc75ede
jaf <code_writer@163.com>
2016-08-08 20:31:20 +0800

    addBlog.jsp BlogControlelr.js

--- src/main/java/com/mouge/controller/BlogController.java
+++ src/main/java/com/mouge/controller/BlogController.java
@@ -1,10 +1,13 @@
 package com.mouge.controller;
 
 import com.mouge.model.BlogEntity;
+import com.mouge.model.UserEntity;
 import com.mouge.repository.BlogRepository;
+import com.mouge.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,13 +21,36 @@ public class BlogController {
 
     @Autowired
     BlogRepository blogRepository;
+    @Autowired
+    UserRepository userRepository;
 
     //查看所有博文
     @RequestMapping(value = "/admin/blogs", method = RequestMethod.GET)
     public String showBlogs(ModelMap modelMap) {
         List<BlogEntity> blogList = blogRepository.findAll();
         modelMap.addAttribute("blogList", blogList);
-        return "admain/blogs";
+        return "admin/blogs";
+    }
+
+    //添加博文
+    @RequestMapping(value = "/admin/blogs/add", method = RequestMethod.GET)
+    public String addBlog(ModelMap modelMap) {
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("userList", userList);
+        return "admin/addBlog";
+    }
+
+    // 添加博文，POST请求，重定向为查看博客页面
+    @RequestMapping(value = "/admin/blogs/addP", method = RequestMethod.POST)
+    public String addBlogPost(@ModelAttribute("blog") BlogEntity blogEntity) {
+        // 打印博客标题
+        System.out.println(blogEntity.getTitle());
+        // 打印博客作者
+        System.out.println(blogEntity.getUserByUserId().getNickname());
+        // 存库
+        blogRepository.saveAndFlush(blogEntity);
+        // 重定向地址
+        return "redirect:/admin/blogs";
     }
 
 }
