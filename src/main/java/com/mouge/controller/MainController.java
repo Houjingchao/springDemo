dffec52cc98749c96dcb46008276311d1024cacf
jaf <code_writer@163.com>
2016-08-08 12:54:39 +0800

    addUser.jsp MainController.java update

--- src/main/java/com/mouge/controller/MainController.java
+++ src/main/java/com/mouge/controller/MainController.java
@@ -5,6 +5,7 @@ import com.mouge.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
@@ -27,10 +28,37 @@ public class MainController {
         return "index";
     }
 
-    @RequestMapping(value = "/admain/users", method = RequestMethod.GET)
+    @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
     public String getUsers(ModelMap modelMap) {
+        // 查询user表中所有记录
         List<UserEntity> userList = userRepository.findAll();
+
+        // 将所有记录传递给要返回的jsp页面，放在userList当中
         modelMap.addAttribute("userList", userList);
-        return "admain/users";
+
+        // 返回pages目录下的admin/users.jsp页面
+        return "admin/users";
     }
+
+    @RequestMapping(value = "/admain/users/add",method = RequestMethod.GET)
+    public String addUser() {
+        return "admain/addUser";
+    }
+
+    // post请求，处理添加用户请求，并重定向到用户管理页面
+    @RequestMapping(value = "/admin/users/addP", method = RequestMethod.POST)
+    public String addUserPost(@ModelAttribute("user") UserEntity userEntity) {
+        // 注意此处，post请求传递过来的是一个UserEntity对象，里面包含了该用户的信息
+        // 通过@ModelAttribute()注解可以获取传递过来的'user'，并创建这个对象
+
+        // 数据库中添加一个用户，该步暂时不会刷新缓存
+        //userRepository.save(userEntity);
+
+        // 数据库中添加一个用户，并立即刷新缓存
+        userRepository.saveAndFlush(userEntity);
+
+        // 重定向到用户管理页面，方法为 redirect:url
+        return "redirect:/admin/users";
+    }
+
 }
