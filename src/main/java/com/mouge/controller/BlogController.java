1b5512ed1b2bb8cd23df6b8fbca362db7dd25c70
jaf <code_writer@163.com>
2016-08-09 09:36:53 +0800

    BlogDeatil.jsp BlogController.java

--- src/main/java/com/mouge/controller/BlogController.java
+++ src/main/java/com/mouge/controller/BlogController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -50,6 +51,44 @@ public class BlogController {
         // 存库
         blogRepository.saveAndFlush(blogEntity);
         // 重定向地址
+        return "redirect:/admin/blogs";
+    }
+
+    // 查看博文详情，默认使用GET方法时，method可以缺省
+    @RequestMapping("/admin/blogs/show/{id}")
+    public String showBlog(@PathVariable("id") int id, ModelMap modelMap) {
+        BlogEntity blog = blogRepository.findOne(id);
+        modelMap.addAttribute("blog", blog);
+        return "admin/blogDetail";
+    }
+
+
+    // 修改博文内容，页面
+    @RequestMapping("/admin/blogs/update/{id}")
+    public String updateBlog(@PathVariable("id") int id, ModelMap modelMap) {
+        // 是不是和上面那个方法很像
+        BlogEntity blog = blogRepository.findOne(id);
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("blog", blog);
+        modelMap.addAttribute("userList", userList);
+        return "admin/updateBlog";
+    }
+
+    // 修改博客内容，POST请求
+    @RequestMapping(value = "/admin/blogs/updateP", method = RequestMethod.POST)
+    public String updateBlogP(@ModelAttribute("blogP") BlogEntity blogEntity) {
+        // 更新博客信息
+        blogRepository.updateBlog(blogEntity.getTitle(), blogEntity.getUserByUserId().getId(),
+                blogEntity.getContent(), blogEntity.getPubDate(), blogEntity.getId());
+        blogRepository.flush();
+        return "redirect:/admin/blogs";
+    }
+
+    // 删除博客文章
+    @RequestMapping("/admin/blogs/delete/{id}")
+    public String deleteBlog(@PathVariable("id") int id) {
+        blogRepository.delete(id);
+        blogRepository.flush();
         return "redirect:/admin/blogs";
     }
 
